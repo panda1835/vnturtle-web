6ebfa79bd225277cc852bce168112415ee4c6541
panda1835 <lehoangphuc1820@gmail.com>
2023-09-26 11:43:16 +0700

    initial commits

---
+++ assets/content/about_us.md
@@ -0,0 +1,39 @@
+# Về chúng tôi
+
+Chào mừng bạn đến với [Tên Ứng Dụng Của Bạn] - Ứng Dụng Bảo Vệ Rùa Nước Tại Việt Nam!
+
+
+
+## Sứ Mệnh Của Chúng Tôi
+
+Tại [Tên Ứng Dụng Của Bạn], sứ mệnh của chúng tôi rất rõ ràng: bảo vệ và bảo tồn rùa nước và rùa cạn tại Việt Nam thông qua sức mạnh của công nghệ và hành động từ cộng đồng. Chúng tôi đam mê bảo tồn những sinh vật tuyệt vời này cho thế hệ tương lai.
+
+## Câu Chuyện Của Chúng Tôi
+
+[Thêm một câu chuyện ngắn về việc ứng dụng ra đời như thế nào và tại sao. Chia sẻ động cơ cá nhân và hành trình của bạn trong việc tạo ra ứng dụng.]
+
+## Gặp Gỡ Đội Ngũ
+
+[Thêm thông tin về các thành viên chính của đội ngũ, vị trí công việc của họ và lý lịch ngắn gọn. Đặc biệt là nêu rõ kiến thức và kinh nghiệm của họ trong lĩnh vực bảo tồn động vật hoang dã, công nghệ hoặc các lĩnh vực liên quan.]
+
+## Đối Tác và Hợp Tác
+
+[Nêu rõ bất kỳ đối tác hoặc sự hợp tác nào với tổ chức, cơ quan chính phủ hoặc các chuyên gia đóng góp vào sự hiệu quả và uy tín của ứng dụng.]
+
+
+## Cam Kết Về Sự Minh Bạch
+
+[Giải thích cam kết của bạn đối với việc làm cho ứng dụng hoạt động một cách trong suốt và cách xử lý dữ liệu của người dùng. Đảm bảo người dùng rằng sự đóng góp của họ làm thay đổi thực sự.]
+
+## Kế Hoạch Tương Lai
+
+[Chia sẻ tầm nhìn của bạn về tương lai của ứng dụng và các mục tiêu bạn định đạt được trong những năm tới. Hãy thông báo cho người dùng biết rằng ứng dụng của bạn ở đây để tồn tại lâu dài.]
+
+## Liên Hệ Với Chúng Tôi
+
+Chúng tôi rất mong nhận được phản hồi từ bạn! Nếu bạn có bất kỳ câu hỏi, đóng góp hoặc yêu cầu hợp tác, vui lòng liên hệ với chúng tôi:
+
+- Email: [Địa chỉ Email Của Bạn]
+- Facebook: [Liên kết đến Trang Facebook Của Bạn]
+- Twitter: [Liên kết đến Hồ sơ Twitter Của Bạn]
+- Instagram: [Liên kết đến Hồ sơ Instagram Của Bạn]
---
+++ flutter_service_worker.js
@@ -0,0 +1,234 @@
+'use strict';
+const MANIFEST = 'flutter-app-manifest';
+const TEMP = 'flutter-temp-cache';
+const CACHE_NAME = 'flutter-app-cache';
+
+const RESOURCES = {"version.json": "772297075a890345dbe7e76543e54777",
+"index.html": "9e71e25ead2883926e060688f8cfb71c",
+"/": "9e71e25ead2883926e060688f8cfb71c",
+"main.dart.js": "cba59cf92f12bf0cf36f3b18026f644d",
+"flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"manifest.json": "b3cf7149588cedc52e5ed383746fdf9c",
+"assets/images/reference_images/Amyda_ornata/Amyda_ornata_thumbnail.jpg": "e8ac3c4f372fa5fec167ff798e75216a",
+"assets/images/reference_images/Mauremys_mutica/Mauremys_mutica_thumbnail.jpg": "a3dd0967cf5259f7cf604d142132aba1",
+"assets/images/reference_images/Cuora_picturata/Cuora_picturata_thumbnail.jpg": "33619f7a1ff5a88d478e9f54a2012b2d",
+"assets/images/reference_images/Cuora_amboinensis/Cuora_amboinensis_thumbnail.jpg": "64aa6b77b120d7d4310c3cf2c82b127c",
+"assets/images/reference_images/Cuora_galbinifrons/Cuora_galbinifrons_thumbnail.jpg": "45b1fbed81ed211f0c1260843ab9b7bf",
+"assets/images/reference_images/Malayemys_subtrijuga/Malayemys_subtrijuga_thumbnail.jpg": "c25990d8d35eeab114f48a2dd2244334",
+"assets/images/reference_images/Cuora_cyclornata/Cuora_cyclornata_thumbnail.jpg": "002a7d12ab03e3982527f8f273efad7b",
+"assets/images/reference_images/Pelochelys_cantorii/Pelochelys_cantorii_thumbnail.jpg": "7474e3bb4df9afba946f8c431ada8743",
+"assets/images/reference_images/Cuora_mouhotii/Cuora_mouhotii_thumbnail.jpg": "0303ef6d39887537a959f62ea3831697",
+"assets/images/reference_images/Platysternon_megacephalum/Platysternon_megacephalum_thumbnail.jpg": "7e57b37573ea0b0b6086f9766a860238",
+"assets/images/reference_images/Geoemyda_spengleri/Geoemyda_spengleri_thumbnail.jpg": "3d3eeb09b37d08f682094422b65bdab1",
+"assets/images/reference_images/Heosemys_annandalii/Heosemys_annandalii_thumbnail.jpg": "342fee3e5bb8069490123c2e382c1424",
+"assets/images/reference_images/Cyclemys_oldhamii/Cyclemys_oldhamii_thumbnail.jpg": "6b11b40985d1ebdf5c5819bcacd4a01f",
+"assets/images/reference_images/Indotestudo_elongata/Indotestudo_elongata_thumbnail.jpg": "a241547f12ca8d0fd4821e1e6848f11c",
+"assets/images/reference_images/Cyclemys_pulchristriata/Cyclemys_pulchristriata_thumbnail.jpg": "434bd51036c2e2319c1e097b53b6995d",
+"assets/images/reference_images/Pelodiscus_variegatus/Pelodiscus_variegatus_thumbnail.jpg": "4e87a94f6fc1d2defd50bace93f2155f",
+"assets/images/reference_images/Palea_steindachneri/Palea_steindachneri_thumbnail.jpg": "f622376901248bed57575a2fe0d741e7",
+"assets/images/reference_images/Rafetus_swinhoei/Rafetus_swinhoei_thumbnail.jpg": "3ca104a52ed450295678316d139cc4ef",
+"assets/images/reference_images/Cyclemys_atripons/Cyclemys_atripons_thumbnail.jpg": "e4d921b110675035fcfb768351962f70",
+"assets/images/reference_images/Sacalia_quadriocellata/Sacalia_quadriocellata_thumbnail.jpg": "7d08481db3075cb826e4dcf4c983fc0a",
+"assets/images/reference_images/Mauremys_annamensis/Mauremys_annamensis_thumbnail.jpg": "aae7afc32eca6a4e3f0d93e0bd167b4c",
+"assets/images/reference_images/Siebenrockiella_crassicollis/Siebenrockiella_crassicollis_thumbnail.jpg": "49bacf6c9ab6aca1b7af7e9951ebcb70",
+"assets/images/reference_images/Mauremys_sinensis/Mauremys_sinensis_thumbnail.jpg": "ce6b66a70a1a6a49b6726e95fc201021",
+"assets/images/reference_images/Cuora_bourreti/Cuora_bourreti_thumbnail.jpg": "0fdd6f6e07573b8c4de0a6369d835421",
+"assets/images/reference_images/Heosemys_grandis/Heosemys_grandis_thumbnail.jpg": "100fef9f354177488176ae88e7881687",
+"assets/images/reference_images/Manouria_impressa/Manouria_impressa_thumbnail.jpg": "bbd428093d60d788159c5e9d46623b1b",
+"assets/images/icons/logo.png": "15ae18f81456b625b5d0bf084fcb13a6",
+"assets/images/placeholder-image.jpeg": "8865f01b09e3902c0a7c342fa11a0234",
+"assets/AssetManifest.json": "43ab934c785a168828122e121634870d",
+"assets/content/faq.json": "f2c229ac7d4b7b3638e4a2716c147b65",
+"assets/content/sample_api.json": "70fee31e0bafd5c67835bfa61c287aca",
+"assets/content/species_info_vi.json": "99e0c4c57a258c2f809e4db06a8a157e",
+"assets/content/about_us.md": "0fa7f93ee99cdce462cd8454a7f92bbf",
+"assets/content/conservation_status.json": "a771a74ab21043f4736b3398211b157a",
+"assets/content/species_info_en.json": "ebe309fa2797d2a5ee5c380dfc9839f2",
+"assets/NOTICES": "9ab719535aec52028030477fce251864",
+"assets/FontManifest.json": "bf436384b828ee915a34d6123d1d189f",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "89ed8f4e49bcdfc0b5bfc9b24591e347",
+"assets/shaders/ink_sparkle.frag": "f8b80e740d33eb157090be4e995febdf",
+"assets/AssetManifest.bin": "2a792b62946f0880044e208d75a50a48",
+"assets/fonts/HappyMonkey-Regular.ttf": "e97d8141e3bb9967f810609fc4860ada",
+"assets/fonts/MaterialIcons-Regular.otf": "b4db7444919584beb818c6d59eb79a4a",
+"canvaskit/skwasm.js": "95f16c6690f955a45b2317496983dbe9",
+"canvaskit/skwasm.wasm": "d1fde2560be92c0b07ad9cf9acb10d05",
+"canvaskit/chromium/canvaskit.js": "ffb2bb6484d5689d91f393b60664d530",
+"canvaskit/chromium/canvaskit.wasm": "393ec8fb05d94036734f8104fa550a67",
+"canvaskit/canvaskit.js": "5caccb235fad20e9b72ea6da5a0094e6",
+"canvaskit/canvaskit.wasm": "d9f69e0f428f695dc3d66b3a83a4aa8e",
+"canvaskit/skwasm.worker.js": "51253d3321b11ddb8d73fa8aa87d3b15"};
+// The application shell files that are downloaded before a service worker can
+// start.
+const CORE = ["main.dart.js",
+"index.html",
+"assets/AssetManifest.json",
+"assets/FontManifest.json"];
+
+// During install, the TEMP cache is populated with the application shell files.
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  return event.waitUntil(
+    caches.open(TEMP).then((cache) => {
+      return cache.addAll(
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
+    })
+  );
+});
+// During activate, the cache is populated with the temp files downloaded in
+// install. If this service worker is upgrading from one with a saved
+// MANIFEST, then use this to retain unchanged resource files.
+self.addEventListener("activate", function(event) {
+  return event.waitUntil(async function() {
+    try {
+      var contentCache = await caches.open(CACHE_NAME);
+      var tempCache = await caches.open(TEMP);
+      var manifestCache = await caches.open(MANIFEST);
+      var manifest = await manifestCache.match('manifest');
+      // When there is no prior manifest, clear the entire cache.
+      if (!manifest) {
+        await caches.delete(CACHE_NAME);
+        contentCache = await caches.open(CACHE_NAME);
+        for (var request of await tempCache.keys()) {
+          var response = await tempCache.match(request);
+          await contentCache.put(request, response);
+        }
+        await caches.delete(TEMP);
+        // Save the manifest to make future upgrades efficient.
+        await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        // Claim client to enable caching on first launch
+        self.clients.claim();
+        return;
+      }
+      var oldManifest = await manifest.json();
+      var origin = self.location.origin;
+      for (var request of await contentCache.keys()) {
+        var key = request.url.substring(origin.length + 1);
+        if (key == "") {
+          key = "/";
+        }
+        // If a resource from the old manifest is not in the new cache, or if
+        // the MD5 sum has changed, delete it. Otherwise the resource is left
+        // in the cache and can be reused by the new service worker.
+        if (!RESOURCES[key] || RESOURCES[key] != oldManifest[key]) {
+          await contentCache.delete(request);
+        }
+      }
+      // Populate the cache with the app shell TEMP files, potentially overwriting
+      // cache files preserved above.
+      for (var request of await tempCache.keys()) {
+        var response = await tempCache.match(request);
+        await contentCache.put(request, response);
+      }
+      await caches.delete(TEMP);
+      // Save the manifest to make future upgrades efficient.
+      await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      // Claim client to enable caching on first launch
+      self.clients.claim();
+      return;
+    } catch (err) {
+      // On an unhandled exception the state of the cache cannot be guaranteed.
+      console.error('Failed to upgrade service worker: ' + err);
+      await caches.delete(CACHE_NAME);
+      await caches.delete(TEMP);
+      await caches.delete(MANIFEST);
+    }
+  }());
+});
+// The fetch handler redirects requests for RESOURCE files to the service
+// worker cache.
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  var origin = self.location.origin;
+  var key = event.request.url.substring(origin.length + 1);
+  // Redirect URLs to the index.html
+  if (key.indexOf('?v=') != -1) {
+    key = key.split('?v=')[0];
+  }
+  if (event.request.url == origin || event.request.url.startsWith(origin + '/#') || key == '') {
+    key = '/';
+  }
+  // If the URL is not the RESOURCE list then return to signal that the
+  // browser should take over.
+  if (!RESOURCES[key]) {
+    return;
+  }
+  // If the URL is the index.html, perform an online-first request.
+  if (key == '/') {
+    return onlineFirst(event);
+  }
+  event.respondWith(caches.open(CACHE_NAME)
+    .then((cache) =>  {
+      return cache.match(event.request).then((response) => {
+        // Either respond with the cached resource, or perform a fetch and
+        // lazily populate the cache only if the resource was successfully fetched.
+        return response || fetch(event.request).then((response) => {
+          if (response && Boolean(response.ok)) {
+            cache.put(event.request, response.clone());
+          }
+          return response;
+        });
+      })
+    })
+  );
+});
+self.addEventListener('message', (event) => {
+  // SkipWaiting can be used to immediately activate a waiting service worker.
+  // This will also require a page refresh triggered by the main worker.
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+    return;
+  }
+  if (event.data === 'downloadOffline') {
+    downloadOffline();
+    return;
+  }
+});
+// Download offline will check the RESOURCES for all files not in the cache
+// and populate them.
+async function downloadOffline() {
+  var resources = [];
+  var contentCache = await caches.open(CACHE_NAME);
+  var currentContent = {};
+  for (var request of await contentCache.keys()) {
+    var key = request.url.substring(origin.length + 1);
+    if (key == "") {
+      key = "/";
+    }
+    currentContent[key] = true;
+  }
+  for (var resourceKey of Object.keys(RESOURCES)) {
+    if (!currentContent[resourceKey]) {
+      resources.push(resourceKey);
+    }
+  }
+  return contentCache.addAll(resources);
+}
+// Attempt to download the resource online before falling back to
+// the offline cache.
+function onlineFirst(event) {
+  return event.respondWith(
+    fetch(event.request).then((response) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch((error) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((response) => {
+          if (response != null) {
+            return response;
+          }
+          throw error;
+        });
+      });
+    })
+  );
+}
