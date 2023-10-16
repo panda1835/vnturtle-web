9284c5899dc3b07d1f88dfbf8da7cbc315dd88bd
Panda <52302733+panda1835@users.noreply.github.com>
2023-10-16 14:53:01 +0700

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {"version.json": "772297075a890345dbe7e76543e54777",
 "index.html": "bd6a60b7fb08e3861eac07f1e0c4d29c",
 "/": "bd6a60b7fb08e3861eac07f1e0c4d29c",
-"main.dart.js": "25faf987a1511b37e8c7f754912f30c5",
+"main.dart.js": "2980e0181ab5366507f2aed69b461e11",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "7d672e8731197a2a7887e108d6ecc4f7",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
